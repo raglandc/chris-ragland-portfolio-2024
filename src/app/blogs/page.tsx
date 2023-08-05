b9eec72def4fc11e34164ccf0d3a34b5b9fae23a
Chris Ragland <Chrisragland97@gmail.com>
2023-08-05 13:29:10 -0400

    add new blogs : graphs

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -44,6 +44,7 @@ export default function BlogsPage()
           </div>
         </div>
 
+        {/* Algorithms */}
         <div className="space-y-16">
           <div>
             <div className='mx-auto max-w-7xl px-4 md:px-6'>
@@ -54,8 +55,36 @@ export default function BlogsPage()
             <div 
               className='mx-auto mt-7 flex max-w-7xl snap-x snap-mandatory space-x-6 overflow-x-auto pb-6 lg:mt-8 lg:grid lg:snap-none lg:grid-cols-3 lg:gap-x-3.5 lg:gap-y-12 lg:space-x-0 lg:px-4 before:flex-shrink-0 before:basis-4 before:content-[""] after:flex-shrink-0 after:basis-4 after:content-[""] md:before:basis-6 md:after:basis-6 lg:before:hidden lg:after:hidden'
             >
-              {allBlogs.filter((project) => {
-                return project.postCategory === "Algorithms"
+              {allBlogs.filter((post) => {
+                return post.postCategory === "Algorithms"
+              }).map((blog) => {
+                return (
+                  <BlogCard 
+                    key={blog.title}
+                    title={blog.title}
+                    link={blog.link}
+                    readTime={blog.readTime}
+                    image={blog.image}
+                  />
+                )
+              })}
+            </div>
+          </div>
+        </div>
+
+        {/* Graphs */}
+        <div className="space-y-16">
+          <div>
+            <div className='mx-auto max-w-7xl px-4 md:px-6'>
+              <h2 className='mt-4 max-w-3xl text-xl font-semibold md:text-2xl lg:mt-5'>Graphs</h2>
+              <p className='mt-1 block text-custom-textSecondary lg:text-lg'>Graphs are a data structure used to represent everything from networks to complicated systems</p>
+              <p className='mt-1 block text-custom-textSecondary lg:text-lg'>Learn more about the data structure that makes social networks possible</p>
+            </div>
+            <div 
+              className='mx-auto mt-7 flex max-w-7xl snap-x snap-mandatory space-x-6 overflow-x-auto pb-6 lg:mt-8 lg:grid lg:snap-none lg:grid-cols-3 lg:gap-x-3.5 lg:gap-y-12 lg:space-x-0 lg:px-4 before:flex-shrink-0 before:basis-4 before:content-[""] after:flex-shrink-0 after:basis-4 after:content-[""] md:before:basis-6 md:after:basis-6 lg:before:hidden lg:after:hidden'
+            >
+              {allBlogs.filter((post) => {
+                return post.postCategory === "Graphs"
               }).map((blog) => {
                 return (
                   <BlogCard 
