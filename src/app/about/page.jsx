71821e656b2353602fc56846ac13cedf40d0202a
Chris Ragland <Chrisragland97@gmail.com>
2023-08-05 14:15:45 -0400

    update about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,16 +1,11 @@
 "use client"
 import { useRef } from "react";
+import Image from "next/image";
 import {
   motion,
   useScroll,
   useTransform,
 } from "framer-motion"
-
-import SectionOne from "@/components/about-page-sections/Section-1";
-import SectionTwo from "@/components/about-page-sections/Section-2"
-import SectionThree from "@/components/about-page-sections/Section-3"
-import SectionFour from "@/components/about-page-sections/Section-4"
-import SectionFive from "@/components/about-page-sections/Section-5"
 
 export default function AboutPage()
 {
@@ -23,10 +18,7 @@ export default function AboutPage()
   let opacity = useTransform(scrollYProgress, [0,1], ["100%", "-20%"]);
 
   return (
-    <main className="w-full relative text-white">
-      <div 
-        className="absolute top-0 left-0 right-0 bottom-0 -z-40 bg-black"
-      />
+    <main className="w-full relative">
       {/* introduction */}
       <section 
         ref={introductionRef}
@@ -36,17 +28,33 @@ export default function AboutPage()
           <div className="w-full h-full bg-black/50"/>
         </motion.div>
 
-        <h1 className="font-bold text-custom-colorPrimary text-4xl">Hi, my names Chris</h1>
-        <h2 className="font-bold text-custom-textSecondary text-xl">and this is (part of) my story...</h2>
+        <h1 className="font-bold text-custom-colorPrimary text-4xl md:text-6xl">Hi, my names Chris</h1>
+        <h2 className="font-bold text-custom-textSecondary text-xl md:text-2xl">and this is (part of) my story...</h2>
 
       </section>
 
-      {/* Begin Coding Journey */}
-      <SectionOne />
-      <SectionTwo />
-      <SectionThree />
-      <SectionFour />
-      <SectionFive />
+      <section 
+        className="px-4 md:px-6 max-w-7xl space-y-4 min-h-screen mx-auto"
+      >
+        <Image
+          className="rounded-xl mx-auto"
+          src="/img/about-images/chris.webp"
+          alt="Picture of Chris Ragland"
+          width={250}
+          height={375}
+        />
+        <div className="space-y-3">
+          <h1
+            className="font-semibold text-2xl"
+          >
+            About <span className="text-custom-colorPrimary font-bold">Chris</span>
+          </h1>
+          <p>Chris was born and raised (for the most part) in Indiana. It was not until he turned 19, Chris moved to Florida where he would begin university working towards his dream of solving interesting problems, writing code, and spending his days learning.</p>
+          <p>
+            Chris is currently working towards his bachelors in Computer Science at the University of South Florida.
+          </p>
+        </div>
+      </section>
     </main>
   )
 }
