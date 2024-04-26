455c35d8b16c5ae648ec391db7040e83185e76cf
raglandc <chrisragland97@gmail.com>
2024-04-25 20:31:29 -0400

    Update (Footer) change the w in with to lower case

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
 
   return (
     <footer className="flex flex-col items-center justify-center order-last w-full bg-custom-background h-80 text-custom-textPrimary">
-      <p className="mt-5"> Made With ❤️ by Chris Ragland </p>
+      <p className="mt-5"> Made with ❤️ by Chris Ragland </p>
       <p className="mt-5">&copy; {year} </p>
     </footer>
   )
